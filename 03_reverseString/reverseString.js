d64570dd0892b5d6aed6d0d40d51fb0e93d5a80d
Luiz Luna <luizluna.dev@gmail.com>
2024-09-17 02:06:25 -0300

    Solve exercise 3
    I used a more complex approach than the provided solution
    for the exercise. But i keep it that way because i think
    it serves as a good lesson on how the same thing could be
    done using different levels of complexity if you know the
    right functions.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (text) {
+  const textCharsArray = text.split("");
 
+  let reversedText = "";
+  for (let i = textCharsArray.length - 1; i >= 0; i--) {
+    reversedText += textCharsArray[i];
+  }
+  return reversedText;
 };
 
 // Do not edit below this line
